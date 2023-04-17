5e977a77233ba3c8b92d55a6a9df45e1a5ab576d
kimjin0 <gaius@gmail.com>
2023-04-17 12:12:00 +0900

    마우스 우클릭 기능

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div @contextmenu.prevent="rightClick">마우스 우클릭</div>
   </div>
 </template>
 
@@ -9,6 +10,15 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  setup() {
+    const rightClick = () => {
+      alert('마우스 오른쪽 버튼을 클릭하셨습니다.')
+    }
+
+    return {
+      rightClick
+    }
   }
 }
 </script>
